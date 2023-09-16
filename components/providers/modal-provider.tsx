4e82c5169da4cfbf3abf9f956b0b16118c258f18
Faizanshaikh12 <faizanroy000777@gmail.com>
2023-09-16 22:04:09 +0530

    - Finish Invite Friend Functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import {ThemeProvider as NextThemesProvider} from "next-themes"
 import {ThemeProviderProps} from "next-themes/dist/types"
 import {useEffect, useState} from "react";
 import {CreateServerModal} from "../modals/create-server-modal";
+import {InviteModal} from "../modals/invite-modal";
 
 export function ModalProvider() {
     const [mounted, setMounted] = useState(false);
@@ -16,6 +17,7 @@ export function ModalProvider() {
     return (
         <>
             <CreateServerModal/>
+            <InviteModal/>
         </>
     )
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -9,6 +9,7 @@ import {
     DropdownMenuTrigger
 } from "../ui/dropdown-menu";
 import {ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users} from "lucide-react";
+import {useModal} from "../../hooks/use-model-store";
 
 interface ServerHeaderProps {
     server: ServerWithMemberWithProfile;
@@ -16,6 +17,9 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({server, role}: ServerHeaderProps) => {
+
+    const {onOpen} = useModal();
+
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -29,7 +33,9 @@ export const ServerHeader = ({server, role}: ServerHeaderProps) => {
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
                 {isModerator && (
-                    <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem
+                        onClick={() => onOpen("invite", {server})}
+                        className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
                         Invite People
                         <UserPlus className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
@@ -61,7 +67,7 @@ export const ServerHeader = ({server, role}: ServerHeaderProps) => {
                         <Trash className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
-                {isAdmin && (
+                {!isAdmin && (
                     <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto"/>
