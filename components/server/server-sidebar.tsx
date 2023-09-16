8e31d63d4d65fa45f2e92c14da70e161c6a07d32
Faizanshaikh12 <faizanroy000777@gmail.com>
2023-09-16 20:36:35 +0530

    - finish server sidebar header

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -41,7 +41,7 @@ export const ServerSidebar = async ({serverId}: ServerSidebarProps) => {
 
     if (!server) return redirect('/');
 
-    const role = server.members.find((member) => member.profileId !== profile.id)?.role;
+    const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
     return (
         <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
