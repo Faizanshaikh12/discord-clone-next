11c7079ac8c32bcbceecb5e5844abdce5bcd4708
Faizanshaikh12 <faizanroy000777@gmail.com>
2023-09-24 21:41:47 +0530

    - Chat Scroll Hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {Fragment} from "react"
+import {ElementRef, Fragment, useRef} from "react"
 import {Member, Message, Profile} from "@prisma/client"
 import {ChatWelcome} from "./chat-welcome";
 import {useChatQuery} from "../../hooks/use-chat-query";
@@ -8,6 +8,7 @@ import {Loader2, ServerCrash} from "lucide-react";
 import {ChatItem} from "./chat-item";
 import {format} from "date-fns"
 import {useChatSocket} from "../../hooks/use-chat-socket";
+import {useChatScroll} from "../../hooks/use-chat-scroll";
 
 interface ChatMessagesProps {
     name: string;
@@ -44,10 +45,21 @@ export const ChatMessages = ({
     const addKey = `chat:${chatId}:messages`;
     const updateKey = `chat:${chatId}:messages:update`;
 
-    const {data, status, hashNextPage, fetchNextPage, isFetchingNextPage} =
+    const chatRef = useRef<ElementRef<"div">>(null);
+    const bottomRef = useRef<ElementRef<"div">>(null);
+
+    const {data, status, hasNextPage, fetchNextPage, isFetchingNextPage} =
         useChatQuery({queryKey, apiUrl, paramKey, paramValue})
 
     useChatSocket({queryKey, addKey, updateKey})
+
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0].items?.length ?? 0
+    })
 
     if (status === 'loading') {
         return (
@@ -72,12 +84,27 @@ export const ChatMessages = ({
     }
 
     return (
-        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-            <div className="flex-1"/>
-            <ChatWelcome
-                name={name}
-                type={type}
-            />
+        <div className="flex-1 flex flex-col py-4 overflow-y-auto" ref={chatRef}>
+            {!hasNextPage && <div className="flex-1"/>}
+            {!hasNextPage && (
+                <ChatWelcome
+                    name={name}
+                    type={type}
+                />
+            )}
+            {hasNextPage && (
+                <div className="flex justify-center">
+                    {isFetchingNextPage ? (
+                        <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4"/>
+                    ) : (
+                        <button
+                            onClick={() => fetchNextPage()}
+                            className="text-zinc-500 hover:text-zinc-600
+                        dark:text-zinc-400 dark:hover:text-zinc-300 text-xs my-4 transition
+                        ">Load previous messages</button>
+                    )}
+                </div>
+            )}
             <div className="flex flex-col-reverse mt-auto">
                 {data?.pages?.map((group, i) => (
                     <Fragment key={i}>
--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -1,25 +1,31 @@
-import {useSocket} from "../components/providers/socket-provider";
 import qs from "query-string";
-import {useInfiniteQuery} from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { useSocket } from "@/components/providers/socket-provider";
 
 interface ChatQueryProps {
     queryKey: string;
     apiUrl: string;
     paramKey: "channelId" | "conversationId";
     paramValue: string;
-}
+};
 
-export const useChatQuery = ({queryKey, paramKey, paramValue, apiUrl}: ChatQueryProps) => {
-    const {isConnected} = useSocket();
+export const useChatQuery = ({
+                                 queryKey,
+                                 apiUrl,
+                                 paramKey,
+                                 paramValue
+                             }: ChatQueryProps) => {
+    const { isConnected } = useSocket();
 
-    const fetchMessages = async ({pageParam = undefined}) => {
+    const fetchMessages = async ({ pageParam = undefined }) => {
         const url = qs.stringifyUrl({
             url: apiUrl,
             query: {
                 cursor: pageParam,
-                [paramKey]: paramValue
+                [paramKey]: paramValue,
             }
-        }, {skipNull: true});
+        }, { skipNull: true });
 
         const res = await fetch(url);
         return res.json();
@@ -28,21 +34,21 @@ export const useChatQuery = ({queryKey, paramKey, paramValue, apiUrl}: ChatQuery
     const {
         data,
         fetchNextPage,
-        hashNextPage,
+        hasNextPage,
         isFetchingNextPage,
-        status
+        status,
     } = useInfiniteQuery({
         queryKey: [queryKey],
         queryFn: fetchMessages,
         getNextPageParam: (lastPage) => lastPage?.nextCursor,
-        refetchInterval: isConnected ? false : 1000
-    })
+        refetchInterval: isConnected ? false : 1000,
+    });
 
     return {
         data,
         fetchNextPage,
-        hashNextPage,
+        hasNextPage,
         isFetchingNextPage,
-        status
-    }
+        status,
+    };
 }
